30a0331b4f9f8c52abc0ecacd942ab1e53257d78
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-11 23:17:51 -0300

    Correção no UserContoller @Autowired

--- user-api/src/main/java/com/backendproject/userapi/controller/UserController.java
+++ user-api/src/main/java/com/backendproject/userapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.backendproject.userapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.backendproject.userapi.service.UserService;
 @RestController
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping("/user/")
