8e32e3f42aa5c8754119fe07591d7add69d5f223
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-11 22:19:41 -0300

    Camada de serviços de usuario

---
+++ user-api/src/main/java/com/backendproject/userapi/service/UserService.java
@@ -0,0 +1,59 @@
+package com.backendproject.userapi.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backendproject.userapi.dto.UserDTO;
+import com.backendproject.userapi.model.User;
+import com.backendproject.userapi.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> getAll() {
+        List<User> usuarios = userRepository.findAll();
+        return usuarios.stream().map(UserDTO::convert).collect(Collectors.toList());
+    }
+
+    public UserDTO findById(Long userId) {
+        Optional<User> usuario = userRepository.findById(userId);
+        if(usuario.isPresent()) {
+            return UserDTO.convert(usuario.get());
+        }
+        return null;
+    }
+
+    public UserDTO save(UserDTO userDTO) {
+        User user = userRepository.save(User.convert(userDTO));
+        return UserDTO.convert(user);
+    }
+
+    public UserDTO delete(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()) {
+            userRepository.delete(user.get());
+        }
+        return null;
+    }
+
+    public UserDTO findByCpf(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        if(user != null) {
+            return UserDTO.convert(user);
+        }
+        return null;
+    }
+
+    public List<UserDTO> queryByName(String name) {
+        List<User> usuarios = userRepository.queryByNomeLike(name);
+        return usuarios.stream().map(UserDTO::convert).collect(Collectors.toList());
+    }
+}
