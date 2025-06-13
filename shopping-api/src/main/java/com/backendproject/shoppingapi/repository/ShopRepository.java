e399d1a876d25602622ec2ba7593bc101957d8c8
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-13 18:16:34 -0300

    Camada repository

---
+++ shopping-api/src/main/java/com/backendproject/shoppingapi/repository/ShopRepository.java
@@ -0,0 +1,18 @@
+package com.backendproject.shoppingapi.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.backendproject.shoppingapi.model.Shop;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+    public List<Shop> findAllByUserIdentifier(String userIdentifier);
+
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    public List<Shop> findAllByDateGreaterThanEquals(Date date);
+}
