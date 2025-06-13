58da91c5f19bf830797421f2f45cdddbe7491f5e
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-12 22:50:34 -0300

    Camada de serviços em produtos

---
+++ product-api/src/main/java/com/backendproject/productapi/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.backendproject.productapi.service;
+
+import java.lang.foreign.Linker.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.backendproject.productapi.dto.ProductDTO;
+import com.backendproject.productapi.model.Product;
+import com.backendproject.productapi.repository.ProductRepository;
+
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductDTO> getAll() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(ProductDTO::convert).collect(Collectors.toList());
+    }
+
+    public ProductDTO findByProductIdentifier(String productIdentifier) {
+        Product product = productRepository.findByProductIdentifier(productIdentifier);
+        if(product != null) {
+            return ProductDTO.convert(product);
+        }
+        return null;
+    }
+
+    public ProductDTO save(ProductDTO productDTO) {
+        Product product = productRepository.save(Product.convert(productDTO));
+        return ProductDTO.convert(product);
+    }
+
+    public void delete(Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+        if(product.isPresent()) {
+            productRepository.delete(product.get());
+        }
+    }
+}
