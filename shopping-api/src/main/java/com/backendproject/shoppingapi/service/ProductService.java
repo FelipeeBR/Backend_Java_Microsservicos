6b7ecf07a0d097e443b99e44e214b7528c733a8c
Felipe Mendes Neves <felipemen74@gmail.com>
2025-06-22 21:42:30 -0300

    config deploy apis

--- shopping-api/src/main/java/com/backendproject/shoppingapi/service/ProductService.java
+++ shopping-api/src/main/java/com/backendproject/shoppingapi/service/ProductService.java
@@ -2,26 +2,27 @@ package com.backendproject.shoppingapi.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.backendproject.shoppingclient.dto.ProductDTO;
-import com.backendproject.shoppingclient.exception.UserNotFoundException;
+import com.backendproject.shoppingclient.exception.ProductNotFoundException;
 
+@Service
 public class ProductService {
 
     @Value("${PRODUCT_API_URL:http://localhost:8081/product/}")
     private String productApiURL;
 
-    public ProductDTO getProductByIdentifier(String productIdentifier) { 
+     public ProductDTO getProductByIdentifier(String productIdentifier) {
         try {
-            RestTemplate restTemplate = new RestTemplate(); 
-            String url = productApiURL + productIdentifier; 
-            ResponseEntity<ProductDTO> response = 
-            restTemplate.getForEntity(url, ProductDTO.class); 
-            return response.getBody(); 
+            RestTemplate restTemplate = new RestTemplate();
+            String url = productApiURL + "/product/" + productIdentifier;
+            ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
+            return response.getBody();
         } catch (HttpClientErrorException.NotFound e) {
-            throw new UserNotFoundException();
+            throw new ProductNotFoundException();
         }
     }
 }
